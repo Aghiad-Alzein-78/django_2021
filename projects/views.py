cd16fc9493df4990327c657b4350aa2e21a83b87
Aghiad-Alzein-78 <aghiad_alzein@yahoo.com>
2021-07-21 01:58:46 +0300

    starting forms

--- projects/views.py
+++ projects/views.py
@@ -1,37 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Project
 # Create your views here.
 
-projectList=[
-    {
-        'id':'1',
-        'title':'Ecommerce Website',
-        'description':'Fully functional ecommerce website'
-    },
-    {
-        'id':'2',
-        'title':'Portfolio Website',
-        'description':'This is a portofolio Website'
-    },
-    {
-        'id':'3',
-        'title':'Social Network',
-        'description':'Awsome open source project I am still working'
-    }
-]
 
 def projects(request):
-    page="projects"
-    number=9
-    context = {'page':page,'number':number,'projects':projectList}
+    projects=Project.objects.all()
+    context = {'projects':projects}
    
     return render(request,'projects/projects.html',context)
 
 
 def project(request,pk):
-    projectobj=None
-    for i in projectList:
-        if i['id']==pk:
-            projectobj=i
-    context={''}
-    return render(request,'projects/single-project.html',{'project':projectobj})
+    projectobj=Project.objects.get(id=pk)
+    tags=projectobj.tags.all()
+    context={'project':projectobj,'tags':tags}
+    return render(request,'projects/single-project.html',context)
