70653ed851b7e1a21763240f524dacd6e71be2d5
Aghiad-Alzein-78 <aghiad_alzein@yahoo.com>
2021-08-02 00:34:39 +0300

    Finish forms styling

--- static/uikit/app.js
+++ static/uikit/app.js
@@ -1,4 +1,12 @@
-// Invoke Functions Call on Document Loaded
-document.addEventListener('DOMContentLoaded', function () {
-  hljs.highlightAll();
-});
+let alertWrapper=document.querySelector('.alert')
+let alertClose=document.querySelector('.alert__close')
+
+function hide(){
+  console.log("Clicked!")
+  alertWrapper.style.display='none'
+}
+
+if(alertWrapper){
+  console.log("Inside")
+  alertClose.addEventListener("click",hide)
+}
